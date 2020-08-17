350207131e385f3b7780659f697cb34b301b426b
Nic Ng <1091123@student.ccgs.wa.edu.au>
2020-08-17 14:12:40 +0800

    Attempted to create a countdown timer. Start of code. Not successful.

--- Content Library/Content Library/goals.xaml.cs
+++ Content Library/Content Library/goals.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Timers;
+using System.Windows.Threading;
 
 namespace Content_Library
 {
@@ -30,6 +31,8 @@ namespace Content_Library
         
         System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
 
+        private int time = 0;
+        private DispatcherTimer Timer3;
 
 
         public Goals()
@@ -37,6 +40,10 @@ namespace Content_Library
         {
 
             InitializeComponent();
+            Timer = new DispatcherTimer();
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
 
             Timer.Tick += new EventHandler(Timer_Click);
 
@@ -59,6 +66,35 @@ namespace Content_Library
             clocklabel.Content = $"{d.Hour}:{d.Minute}:{d.Second} here is some more text";
 
         }
+
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            if (time > 10)
+            {
+                if (time <= 10)
+                    if (time % 2 == 0)
+                    {
+                        TBCountDown.Foreground = Brushes.Red;
+                    }
+                    else
+                    {
+                        TBCountDown.Foreground = Brushes.White;
+                    }
+                else
+                {
+                    time--;
+                    TBCountDown.Text = string.Format("00:0{0}:{1}", time / 60, time % 60);
+                }
+
+
+            }
+            else
+            {
+                Timer.Stop();
+                MessageBox.Show("BooW!");
+            }
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
