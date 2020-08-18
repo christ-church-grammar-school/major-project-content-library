d3d316df102deacaf3043afa174ca59a3f8f3f4c
StMelon <65885893+StMelon@users.noreply.github.com>
2020-08-18 10:10:38 +0800

    dont know

--- Content Library/Content Library/goals.xaml.cs
+++ Content Library/Content Library/goals.xaml.cs
@@ -30,9 +30,6 @@ namespace Content_Library
             this.NavigationService.Navigate(new Page2());
         }
 
-        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
-        }
+      
     }
 }
