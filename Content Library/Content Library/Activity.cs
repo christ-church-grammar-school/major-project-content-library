5f38350b8201da32c614bae8f5d8c3e05089e0be
gnolanccgs <gnolan@ccgs.wa.edu.au>
2020-10-16 10:20:23 +0800

    Added new class representations of interface elements Activity and Goal

---
+++ Content Library/Content Library/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content_Library
+{
+	class Activity
+	{
+		public string name;
+		public bool completed;
+
+		public Activity(string n)
+		{
+			name = n;
+			completed = false;
+		}
+	}
+}
---
+++ Content Library/Content Library/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content_Library
+{
+	class Goal
+	{
+		public string title;
+		public List<Activity> activites;
+		public bool completed;
+
+		public Goal(string t)
+		{
+			title = t;
+			activites = new List<Activity>();
+			completed = false;
+		}
+	}
+}
